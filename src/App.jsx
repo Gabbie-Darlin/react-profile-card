41d2f18c551a77e6c8ea3b5795a31030f8bcbff3
Gabbie-Darlin <gabdarlin@gmail.com>
2026-06-11 01:16:33 +0000

    Profile Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import ProfileCard from './components/ProfileCard.jsx';
+
 function App() {
     return(
+    <>
         <h1>First React Application</h1>
+        <ProfileCard />
+    </>
     );
 }
 
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,10 @@
+function ProfileCard() {
+    return(
+        <div>
+            <h2>Gab-Darlin</h2>
+            <p>Fullstack Software Developer</p>
+        </div>
+    );
+}
+
+export default ProfileCard;
