05139188b075de54266cfd9b3339df612805142a
Gabbie-Darlin <gabdarlin@gmail.com>
2026-06-13 09:09:03 +0000

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import ProfileCard from './components/ProfileCard.jsx';
+import ProfileCard from "./components/ProfileCard";
 
 function App() {
     return(
     <>
         <h1>First React Application</h1>
-        <ProfileCard />
+        <ProfileCard 
+            name="Gab-Darlin" 
+            role="Fullstack Software Developer" />
     </>
     );
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,8 +1,8 @@
-function ProfileCard() {
+function ProfileCard(props) {
     return(
         <div>
-            <h2>Gab-Darlin</h2>
-            <p>Fullstack Software Developer</p>
+            <h2>{props.name}</h2>
+            <p>{props.role}</p>
         </div>
     );
 }
